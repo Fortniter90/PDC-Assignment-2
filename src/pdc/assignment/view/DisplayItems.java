d535017c562e305f449e484be8d576952bb0e903
Alias <ethanmgenet@gmail.com>
2024-06-13 03:24:09 +1200

    Final Additions

--- src/pdc/assignment/view/DisplayItems.java
+++ src/pdc/assignment/view/DisplayItems.java
@@ -16,6 +16,7 @@ import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import pdc.assignment.model.Items;
 import pdc.assignment.model.Locations;
+import pdc.assignment.pkg2.HibernateUtil;
 import pdc.assignment.services.ItemInterface;
 import pdc.assignment.services.ItemManagement;
 
@@ -164,6 +165,7 @@ public class DisplayItems extends javax.swing.JPanel {
             //close the main window or exit the application
             Window window = SwingUtilities.getWindowAncestor(this);
             window.dispose(); //closes
+            HibernateUtil.shutdown();
             System.exit(0);
         }
     }//GEN-LAST:event_exitActionPerformed
